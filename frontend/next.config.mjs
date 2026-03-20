77e8ce05c1595cbe3c3a07be7c9f841c115905ef
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2026-03-20 21:24:22 +0100

    add API rewrite rule for CKAN integration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/ckan/:path*',
+                destination: 'http://ckan:5000/api/3/action/:path*',
+            },
+        ]
+    },
 };
 
 export default nextConfig;
