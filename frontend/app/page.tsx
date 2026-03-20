99ec4f00a1ed77ba81ac4d47d4c07775f2c4de63
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2026-03-20 20:32:07 +0100

    clean

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Welcome to the Next.js Frontend!</h1>
-            <p>This is the home page.ss</p>
+            <p>This is the home page.</p>
             <p>You can access the dataset page at <a href="/dataset">/dataset</a>.</p>
         </div>
     );
