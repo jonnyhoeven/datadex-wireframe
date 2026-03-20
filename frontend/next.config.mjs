58bb7ef7c02df3326b8461db927b754ab205454d
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2026-03-20 22:08:50 +0100

    add url switch between dev and containers in docker compose

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
+        const ckanBaseUrl = process.env.CKAN_BASE_URL || 'http://127.0.0.1:5000';
+
         return [
             {
                 source: '/api/ckan/:path*',
-                destination: 'http://ckan:5000/api/3/action/:path*',
+                destination: `${ckanBaseUrl}/api/3/action/:path*`,
             },
         ]
     },
