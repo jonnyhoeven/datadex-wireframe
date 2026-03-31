66981ea4a4e17acde249b0935d2f27a233b084fc
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2026-03-31 03:32:47 +0200

    refactor(ckan): simplify CkanApiTester import

--- frontend/app/ckan/page.tsx
+++ frontend/app/ckan/page.tsx
@@ -1,15 +1,6 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-import React from 'react';
-import {Loader2} from "lucide-react";
-
-const CkanApiTester = dynamic(() => import('../../components/CkanApiTester'), {
-    ssr: false,
-    loading: () => <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-        <Loader2 className="animate-spin text-brand-orange" size={48}/>
-    </div>
-});
+import CkanApiTester from "../../components/CkanApiTester";
 
 export default function CkanPage() {
     return <CkanApiTester/>;
